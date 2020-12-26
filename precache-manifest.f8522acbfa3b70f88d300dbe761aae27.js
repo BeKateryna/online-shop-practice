9f8813dbae9ee216a1220f2dde6d9ae7992d8f6d
Biehantseva Kateryna <begantseva.katerina@gmail.com>
2020-12-26 17:03:45 +0200

    Updates

---
+++ precache-manifest.f8522acbfa3b70f88d300dbe761aae27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d292cfdd24a680685309d22c59331cdb",
+    "url": "/online-shop-practice/index.html"
+  },
+  {
+    "revision": "368ba5fbe9ff1133460a",
+    "url": "/online-shop-practice/static/css/main.7a7a88ca.chunk.css"
+  },
+  {
+    "revision": "3b5cecf366b22fecf1e4",
+    "url": "/online-shop-practice/static/js/2.3151f1a1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/online-shop-practice/static/js/2.3151f1a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368ba5fbe9ff1133460a",
+    "url": "/online-shop-practice/static/js/main.38bff349.chunk.js"
+  },
+  {
+    "revision": "3a77d2f8ac72e241d530",
+    "url": "/online-shop-practice/static/js/runtime-main.1ffc9699.js"
+  },
+  {
+    "revision": "0798a3774e43050a5384f8ba77653c92",
+    "url": "/online-shop-practice/static/media/RC-NEWS-Growing-Smarter-report-released-0219.0798a377.jpg"
+  },
+  {
+    "revision": "afce843d7cadad787b6d3848ff23609c",
+    "url": "/online-shop-practice/static/media/star-1.afce843d.svg"
+  },
+  {
+    "revision": "ca05b0338660aed37e25c6e4c6bbdd4b",
+    "url": "/online-shop-practice/static/media/star-2.ca05b033.svg"
+  }
+]);
